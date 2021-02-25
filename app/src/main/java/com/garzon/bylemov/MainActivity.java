85c1db378df7dfd7590275539410f1e4c4958f87
Rubén Garzón <rubengarzonzafra@gmail.com>
2021-02-25 09:51:21 +0100

    activity moviles creada

--- app/src/main/java/com/garzon/bylemov/MainActivity.java
+++ app/src/main/java/com/garzon/bylemov/MainActivity.java
@@ -2,13 +2,27 @@ package com.garzon.bylemov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.btnIniciar);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        intentMoviles(v);
+    }
+
+    public void intentMoviles(View view) {
+        Intent intent = new Intent(this, Moviles.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/garzon/bylemov/Moviles.java
@@ -0,0 +1,14 @@
+package com.garzon.bylemov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Moviles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moviles);
+    }
+}
